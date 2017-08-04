537c26795c1430392b7c4f7429c377efca01bbb9
golonzovsky <alex.golonzovsky@gmail.com>
2017-08-04 12:33:12 +0200

    initial

---
+++ src/main/java/golonzovsky/paperreader/PaperReaderApplication.java
@@ -0,0 +1,12 @@
+package golonzovsky.paperreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperReaderApplication.class, args);
+	}
+}
---
+++ src/test/java/golonzovsky/paperreader/PaperReaderApplicationTests.java
@@ -0,0 +1,16 @@
+package golonzovsky.paperreader;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaperReaderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
