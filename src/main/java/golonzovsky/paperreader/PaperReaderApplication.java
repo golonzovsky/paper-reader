76dcbd7abfca25c7e2d09c4322a71025026de3a3
golonzovsky <alex.golonzovsky@gmail.com>
2017-08-04 13:29:30 +0200

    format

--- src/main/java/golonzovsky/paperreader/PaperReaderApplication.java
+++ src/main/java/golonzovsky/paperreader/PaperReaderApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaperReaderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaperReaderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaperReaderApplication.class, args);
+    }
 }
